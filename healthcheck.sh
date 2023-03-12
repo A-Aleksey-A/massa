0f6f265adf6f146753a6a65e43ea21d439fc40ce
A-Aleksey-A <126988346+A-Aleksey-A@users.noreply.github.com>
2023-03-12 21:27:02 +0200

    Update healthcheck.sh

--- healthcheck.sh
+++ healthcheck.sh
@@ -1,1 +1,20 @@
+#!/bin/bash
 
+function restart_massa() {
+    echo -e "Restarting Massa ...................................\n"
+    systemctl restart massa
+}
+
+cd $HOME/massa/massa-client
+while true
+do
+        echo -e "Check bundlr validator logs \n"
+        if [ $(journalctl --tail=20 -u massa 2>&1 | grep -c "Send network event failed An error occurred during channel communication: Failed to send event.") -gt 0 ]; then
+            restart_massa
+        fi
+
+        date=$(date +"%H:%M")
+        echo "Last Update: ${date}"
+        printf "Sleep 1 hour\n"
+        sleep 1h
+done
